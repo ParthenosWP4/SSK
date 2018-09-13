9fd34268afa0b68977159b9628ad3dc03d1a108a
Lionel TADJOU <lioneltadjou@gmail.com>
2018-09-13 17:32:35 +0200

    Update code so that display match the one on d4Science

--- SSK-FrontEnd-v3/.history/src/environments/environment.prod_20180912150410.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  //sskBackendEndpoint : 'https://ssk-application.parthenos.d4science.org/ssk_services-0.0.1/',
-  sskBackendEndpoint : 'http://ssk.huma-num.fr/ssk_services-0.0.1/'
-  //sskBackendEndpoint : 'http://localhost:9000/ssk_services-0.0.1/',
-  //standardEndpoint : 'it.dariah.eu:8983/solr/WP4/select?indent=on&q=*:*&rows=100&wt=json'
-};
