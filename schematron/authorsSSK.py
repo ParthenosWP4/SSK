ecc678d7cbd60fc117736b72dddac77aefbc4a95
charlesriondet <charles.riondet@inria.fr>
2018-12-12 15:43:11 +0100

    List of Authors and scenarios in CSV

---
+++ schematron/authorsSSK.py
@@ -0,0 +1,53 @@
+#coding: utf-8
+
+import subprocess
+import re
+import os
+from lxml import etree as ET
+from bs4 import BeautifulSoup
+from datetime import datetime
+import csv
+import sys
+
+from schematron.ssk import schSSK
+
+ssk = schSSK()
+
+authorsList= []
+scenarios = ssk.get_files("../scenarios")
+
+lengthCal = []
+for scenario in scenarios:
+    parsed = ssk.loadBS(scenario)
+    scAut = {
+        "scenarioId": parsed.TEI.attrs["xml:id"],
+        "scenarioURL": "https://github.com/ParthenosWP4/SSK/tree/master/"+ scenario[3:],
+        "scenarioTitle": parsed.find("title").text
+    }
+    authors = parsed.find_all("author")
+    for i in range(len(authors)):
+        authName = authors[i].persName.text
+        authAff = authors[i].affiliation.text
+        authorKey = "author" + str(i+1)
+        scAut[authorKey] = authName + " (" + authAff + ")"
+    lengthCal.append(len(scAut))
+    authorsList.append(scAut)
+
+maxi = max(lengthCal)
+
+#add additional empty fields for the csv
+for scen in authorsList:
+    authNumb = len(scen) - 3
+    distance = maxi - len(scen)
+    if distance > 0:
+        for i in range(distance):
+            newKey = "author" + str(authNumb + 1 + i)
+            scen[newKey] = ""
+
+output=os.getcwd()+"/authors.csv"
+with open(output, "w") as f:
+
+    keys = authorsList[maxi].keys()
+    dict_writer = csv.DictWriter(f, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(authorsList)
---
+++ schematron/ssk.py
@@ -0,0 +1,92 @@
+#coding: utf-8
+
+import re
+import os
+from lxml import etree as ET
+from bs4 import BeautifulSoup
+import csv
+
+class schSSK:
+
+    def create_directory(self, directory):
+        """Create a new directory.
+        :param directory: path to new directory
+        :type directory: string
+        """
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    # Manage input files to handle
+    def get_files(self, d):
+        filesList = []  # liste fichiers
+        for fileName in os.listdir(d):
+            if fileName.endswith(".xml"):
+                filesList.append(d + "/" + fileName)
+        return filesList
+
+    def loadBS(self, xmlfile):
+        with open(xmlfile) as file:
+            testedFile = BeautifulSoup(file, 'xml')
+            return testedFile
+
+    def loadTree(self, xmlfile):
+        parser = ET.XMLParser(ns_clean=True)
+        tree = ET.parse(xmlfile, parser)
+        return tree
+
+    def parseSVRL(self, svrl, tree):
+        diagnostic = []
+        fired = svrl.find_all('failed-assert')
+        successfulReports = svrl.find_all('successful-report')
+        fired.extend(successfulReports)
+        for fire in fired:
+            location = ssk.getLocations(fire.attrs['location'], tree)
+            if location[1] is not None:
+                lineNumber = location[1].sourceline
+                tagName = location[1].tag
+                tagText = location[1].text
+            else:
+                lineNumber = ""
+                tagName = ""
+                tagText = ""
+
+            message = " ".join(fire.text.split())
+            rule = {
+                   # "context": fire.findPrevious('fired-rule')['context'],
+                    #"test": fire['test'],
+                    "location": location[0],
+                    "line": lineNumber,
+                    #"tag" : tagName,
+                    # "attributes" : location[1].attrib,
+                    #"nodeText": tagText,
+                    "message": message
+                }
+            diagnostic.append(rule)
+        return diagnostic
+
+    def getLocations(self, assertLocation, tree):
+        # patters to process the xpathes
+        pattern1 = re.compile('/\*:')
+        pattern2 = re.compile('\[namespace-uri\(\)=\'http://www\.tei\-c\.org/ns/1\.0\'\]')
+        pattern3 = re.compile('/')
+
+        location1 = re.sub(pattern1, '/', assertLocation)
+        location2 = re.sub(pattern2, '', location1)
+        # Different processings if the context node is root or not
+        if len(location2) > 7:
+            locationNorm = re.sub(pattern3, '/{http://www.tei-c.org/ns/1.0}', location2[7:])[1:]
+        else:
+            locationNorm = re.sub(pattern3, '/{http://www.tei-c.org/ns/1.0}', location2)[1:]
+
+        location = tree.getroot().find(locationNorm)
+        return location2, location
+
+    def writeCSV(self, diagnostic, report, reportFolder):
+        keys = diagnostic[0].keys()
+        reportFile = re.search('\/(.+?)\.xml', report).group(1) + "_report.csv"
+        csvFile = reportFolder + "/" + os.path.basename(os.path.normpath(reportFile))
+
+        with open(csvFile, 'w') as output_file:
+            dict_writer = csv.DictWriter(output_file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(diagnostic)
