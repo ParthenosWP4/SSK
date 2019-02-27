510c1c78365f3b9f599fa662e258977da7d56bff
charlesriondet <charles.riondet@inria.fr>
2019-02-27 17:33:15 +0100

    add processing scripts for deliverable 44

---
+++ dataProcessing/api.py
@@ -0,0 +1,33 @@
+#coding: utf-8
+
+import subprocess
+import os
+import sys
+from bs4 import BeautifulSoup
+from datetime import datetime
+import csv
+from ssk import schSSK
+import requests
+import urllib.request as req
+
+ssk = schSSK()
+
+url = "http://ssk.paris.inria.fr:8080/ssk_services-0.0.1/api/scenarios?id="
+scenarios = ssk.get_files("../scenarios")
+folder = "../API/scenariosExport/"
+ssk.create_directory(folder)
+for scenario in scenarios:
+    id = str(scenario)[13:-4]
+    r = requests.get(url+id)
+    file = folder + id + ".xml"
+    with open(file, 'wb') as f:
+        f.write(r.content)
+
+source = '-s:'+folder
+outFolder = "../API/print/"
+ssk.create_directory(outFolder)
+out = '-o:'+ outFolder
+tei2print = subprocess.run(['saxon', source, '-xsl:../API/scenario2print.xsl', out])
+prints = ssk.get_files(outFolder)
+for printo in prints:
+    print2docx = subprocess.run(['../spec/Stylesheets/bin/teitodocx', printo])
--- dataProcessing/standardsProcess.py
+++ dataProcessing/standardsProcess.py
@@ -1,3 +1,24 @@
 import json
 import csv
+from ssk import schSSK
 
+f = open('../standardsKB/standards.json')
+data = json.load(f)
+f.close()
+
+entries = ("id", "standard_desc_fr", "standard_desc_deu", "standard_desc_esp", "standard_type", "standard_data_type", "standard_desc_ita", "standard_tags", "standard_resources", "_version_")
+
+def entries_to_remove(entries, the_dict):
+    for key in entries:
+        if key in the_dict:
+            del the_dict[key]
+
+for dic in data:
+    entries_to_remove(entries, dic)
+
+keys = data[0].keys()
+
+with open("standards.csv", 'w') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(data)
